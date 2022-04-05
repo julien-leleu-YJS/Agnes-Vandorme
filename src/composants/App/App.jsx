f315eb343cc163a70caef52c148af309d246d81a
julien-leleu-YJSgit config --global user.email jleleu.dev@gmail.com <jleleu.dev@gmail.com>
2022-04-05 20:09:04 +0200

    routes init

--- src/composants/App/App.jsx
+++ src/composants/App/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import {
+  Routes, Route,
+} from 'react-router';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
 
-      app
+      <Routes>
+        <Route path="/" element="entrance" />
+        <Route path="/home" element="home" />
+        <Route path="/biography" element="biography" />
+        <Route path="/artist-web-galery" element="artist-web-galery" />
+        <Route path="/work-art" element="work-art" />
+        <Route path="/contact" element="contact" />
+        <Route path="/special-mentions" element="special-mentions" />
+      </Routes>
 
     </div>
   );
