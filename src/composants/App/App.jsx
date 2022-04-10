301d26db99558d3a459a21879af8b25c415839e2
julien-leleu-YJSgit config --global user.email jleleu.dev@gmail.com <jleleu.dev@gmail.com>
2022-04-10 19:27:49 +0200

    entrance page with style

--- src/composants/App/App.jsx
+++ src/composants/App/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element="Entrance" />
+        <Route path="/" element={<Entrance />} />
 
       </Routes>
       {window.location.pathname !== '/' && ('header ')}
--- src/pages/entrance/Entrance.jsx
+++ src/pages/entrance/Entrance.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './entrance.scss';
 
 function Entrance() {
   return (
-    <div>
+    <div className="title-container">
       <h1 className="title-entrance">Agnès Vandorme</h1>
-      <div />
+      <div className="separation">  </div>
       <h2 className="second-title-entrance">Artist Web Galery</h2>
       <button className="enter-button" type="button">enter</button>
     </div>
