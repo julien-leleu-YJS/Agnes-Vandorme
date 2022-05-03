88c25f443ee1237a9fcb56ce9cc36b70b243331c
julien-leleu-YJSgit config --global user.email jleleu.dev@gmail.com <jleleu.dev@gmail.com>
2022-05-03 16:49:22 +0200

    contact form linked to the receiving email address

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,40 +1,56 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 import './contact.scss';
 // utiliser ce plug-in pour le formulaire de contact https://formspree.io/login
+
 function Contact() {
+  const [state, handleSubmit] = useForm('mrgjojda');
+  if (state.succeeded) {
+    return <p>Merci pour votre message !</p>;
+  }
   return (
     <div className="contact-container">
       <h1 className="title-contact">Contact</h1>
-      <div className="form-container">
-        <form onSubmit={onclick}>
-          <fieldset>
-            <label>
-              <p className="title-input">Nom</p>
-              <input className="input" name="name" />
-            </label>
-            <label>
-              <p className="title-input">Pénom</p>
-              <input className="input" name="surname" />
-            </label>
-            <label>
-              <p className="title-input">email</p>
-              <input className="input" name="email" />
-            </label>
-            <label>
-              <p className="title-input">Numéro d&#39;Oeuvre(s)</p>
-              <input className="input" name="number" />
-            </label>
-            <label>
-              <p className="title-input">Message</p>
-              <input className="message" type="text" maxLength="120" name="message" />
-            </label>
+      <form className="form-container" onSubmit={handleSubmit}>
+        <label>
+          <p className="title-input">Nom</p>
+          <input className="input" name="name" />
+        </label>
+        <label>
+          <p className="title-input">Pénom</p>
+          <input className="input" name="surname" />
+        </label>
 
-          </fieldset>
-          <button className="submit" type="submit">Envoyer</button>
-
-        </form>
-      </div>
+        <label className="title-input" htmlFor="email">
+          Email Address
+        </label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+        />
+        <ValidationError
+          prefix="Email"
+          field="email"
+          errors={state.errors}
+        />
+        <label className="title-input" htmlFor="message">
+          Votre message
+        </label>
+        <textarea
+          id="message"
+          name="message"
+        />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+        <button className="submit" type="submit" disabled={state.submitting}>
+          Envoyer
+        </button>
+      </form>
     </div>
   );
 }
